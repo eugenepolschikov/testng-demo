e56afe6335a0e8e21a215752dc4f762366a09d4f
eugene polschikov <eugene.polschikov@gmail.com>
2019-04-11 17:58:00 +0300

    Finalized logic in GoogleLandingPage page object.

--- src/main/java/com/testngdemo/automation/pages/GoogleLandingPage.java
+++ src/main/java/com/testngdemo/automation/pages/GoogleLandingPage.java
@@ -4,11 +4,17 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import ru.yandex.qatools.allure.annotations.Step;
 
-import static com.testngdemo.automation.webdriver.ElementsUtil.*;
+import java.util.List;
+
+import static com.testngdemo.automation.utils.CaptureSnapshot.captureSnapshotForAllure;
+import static com.testngdemo.automation.webdriver.ElementsUtil.waitForElementToBeClickable;
+import static com.testngdemo.automation.webdriver.ElementsUtil.waitForPageLoaded;
 
 /**
- * Google landing page object: webelements and methods operating on the provided webelements
+ * Google landing page object: web-elements and methods operating on the provided webelements
  *
  * @author Eugene Polschikov
  * @date 2019-04-11
@@ -17,29 +23,34 @@ public class GoogleLandingPage extends Page {
     public GoogleLandingPage(WebDriver driver) {
         super(driver);
     }
+
     private final static String SEARCH_INPUT_CSS = "input[name=\"q\"]";
+    private final static String GOOGLE_SEARCH_RESULT_LIST_CSS = "div[id*=\"ires\"] div.rc>div.r>a[href]";
     @FindBy(css = SEARCH_INPUT_CSS)
     private WebElement searchInput;
+    @FindBy(css = GOOGLE_SEARCH_RESULT_LIST_CSS)
+    private List<WebElement> googleSearchResults;
 
-    public GoogleLandingPage enterQueryToSearchFor(String googleQuery){
+    public GoogleLandingPage enterQueryToSearchFor(String googleQuery) {
         log.info("entering query to search in google input");
         waitForPageLoaded(driver);
-        waitForElementToBeClickable(driver,searchInput);
+        waitForElementToBeClickable(driver, searchInput);
         searchInput.clear();
         searchInput.sendKeys(googleQuery);
         return this;
     }
 
-    public GoogleLandingPage doTheSearchAfterQueryEntered(){
+    public GoogleLandingPage doTheSearchAfterQueryEntered() {
         log.info("sending 'Enter' key");
         searchInput.sendKeys(Keys.ENTER);
         waitForPageLoaded(driver);
         return new GoogleLandingPage(driver);
     }
 
-    public GoogleLandingPage ensureThatAtLeastOneSearchResultAppeared(){
-
-
-        return new GoogleLandingPage(driver);
+    @Step("analyzing found results and doing screenshot of the page")
+    public void ensureThatAtLeastOneSearchResultAppeared() {
+        log.info("checking that at least 1 result is displayed in search");
+        Assert.assertTrue(googleSearchResults.size() > 0, "ooops, looks nothing have been found in search. Please contact test developers for investigation");
+        captureSnapshotForAllure(driver);
     }
 }
