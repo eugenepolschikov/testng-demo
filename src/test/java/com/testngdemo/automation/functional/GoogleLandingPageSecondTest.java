e34ebc4acbf481de5e9cfd2590dffcef400c8cc3
eugene polschikov <eugene.polschikov@gmail.com>
2019-04-12 08:45:11 +0300

    Listeners annotation in test class.

--- src/test/java/com/testngdemo/automation/functional/GoogleLandingPageSecondTest.java
+++ src/test/java/com/testngdemo/automation/functional/GoogleLandingPageSecondTest.java
@@ -1,10 +1,15 @@
 package com.testngdemo.automation.functional;
 
+import com.testngdemo.automation.listeners.paralleltests.InvokedMethodListenerWithScreenshots;
+import com.testngdemo.automation.listeners.retrymechanism.RetryListener;
+import org.testng.annotations.Listeners;
+
 /**
  * Second test suite for demo testNG purposes
  *
  * @author Eugene Polschikov
  * @date 2019-04-11
  */
+@Listeners({ InvokedMethodListenerWithScreenshots.class, RetryListener.class })
 public class GoogleLandingPageSecondTest extends TestSuitesBase {
 }
